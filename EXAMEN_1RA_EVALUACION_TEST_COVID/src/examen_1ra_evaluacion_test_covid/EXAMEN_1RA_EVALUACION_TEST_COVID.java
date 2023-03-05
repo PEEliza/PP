831e48f23e1803f9d15b184a56c49c4134bb654f
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-03-04 23:14:17 -0600

    Add files via upload

---
+++ EXAMEN_1RA_EVALUACION_TEST_COVID/src/examen_1ra_evaluacion_test_covid/EXAMEN_1RA_EVALUACION_TEST_COVID.java
@@ -0,0 +1,14 @@
+
+package examen_1ra_evaluacion_test_covid;
+
+
+public class EXAMEN_1RA_EVALUACION_TEST_COVID {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestCovid MiTest = new TestCovid (25, false, 70.5, 1.80);
+        MiTest.imprimirDatos();
+    }
+    
+}
